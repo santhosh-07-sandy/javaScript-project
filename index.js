dbf6c8fd44f7183bbfe9f24940b831df8aa51043
santhosh-07-sandy <santhosh1917santo@gmail.com>
2024-11-22 21:09:47 +0530

    deleted all files from git_branch_1

--- index.js
+++
@@ -1,9 +0,0 @@
-// Project Name:cm to inches
-function convert()
-    {
-       let cmVal = Number(document.getElementById("input").value)
-       let inchVal = cmVal/2.5
-
-       let result = document.getElementById("result")
-       result.innerHTML = inchVal.toFixed(2) + " inches"
-     }
